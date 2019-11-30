630d8aff77a6e55e21a82f49e7cc234b15613a48
idmyn <hello@davidmyno.rs>
2019-11-30 12:36:03 +0000

    set up database connection

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+mongoose.connect("mongodb://localhost/contentStream", { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
