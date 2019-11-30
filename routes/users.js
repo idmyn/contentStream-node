864f77b3ada3b639ab0ffa8b33d44933b509de16
idmyn <hello@davidmyno.rs>
2019-11-30 13:12:16 +0000

    set up index and create routes for user

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,37 @@
+const argon2 = require('argon2')
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 
 // index
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+// create
+router.post('/', async (req, res) => {
+  const passwordHash = await argon2.hash(req.body.password)
+
+  const user = new User({
+    email: req.body.email,
+    password: passwordHash
+  })
+
+  try {
+    const newUser = await user.save()
+    res.status(201).json(newUser)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 })
 
 // show
 router.get('/:id', (req, res) => {
-})
-
-// create
-router.post('/', (req, res) => {
 })
 
 // update
