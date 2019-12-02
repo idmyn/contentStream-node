d592bff5a0a9f0ef452f2e6c12163c3c9e1dbaec
idmyn <hello@davidmyno.rs>
2019-12-02 11:46:44 +0000

    refactor twitter.js ready to abstract API methods to API module

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,27 +1,80 @@
-const express = require('express')
-const router = express.Router()
-// const Post = require('../models/post')
+require('dotenv').config()
 const request = require('request-promise-native')
 const Twitter = require('twitter')
 const encodeUrl = require('encodeurl')
-require('dotenv').config()
+const express = require('express')
+const router = express.Router()
+// const API = require('../api/twitter')
+
 const consumerKey = process.env.TWITTER_KEY
 const consumerSecret = process.env.TWITTER_SECRET
-let tmpOauthToken
+
 let tmpOauthTokenSecret
-// const Api = require('../api/config')
+
+const twitterHeaders = {
+  Accept: '*/*',
+  Connection: 'close',
+  'User-Agent': 'node-twitter/1'
+}
+
+const buildAuthURL = () => {
+  return request.post({
+    twitterHeaders,
+    oauth: {
+      consumer_key: consumerKey,
+      consumer_secret: consumerSecret,
+      callback: encodeUrl('http://localhost:3001/twitter/success')
+    },
+    url: 'https://api.twitter.com/oauth/request_token'
+  }).then(response => {
+    const tmpOauthToken = response.split('&')[0].split('=')[1]
+    tmpOauthTokenSecret = response.split('&')[1].split('=')[1]
+    return 'https://api.twitter.com/oauth/authorize?oauth_token=' + tmpOauthToken
+  })
+}
+
+const requestTwitterCreds = (
+  tmpOauthToken,
+  oauthVerifier,
+  tmpOauthTokenSecret
+) => {
+  return request.post({
+    twitterHeaders,
+    oauth: {
+      consumer_key: consumerKey,
+      consumer_secret: consumerSecret,
+      token: tmpOauthToken,
+      token_secret: tmpOauthTokenSecret,
+      verifier: oauthVerifier
+    },
+    url: 'https://api.twitter.com/oauth/access_token'
+  }).then(response => {
+    const oauthKey = response.split('&')[0].split('=')[1]
+    const oauthSecret = response.split('&')[1].split('=')[1]
+    return { oauthKey, oauthSecret }
+  })
+}
+
+const buildTwitterClient = (creds) => {
+  return new Twitter({
+    consumer_key: consumerKey,
+    consumer_secret: consumerSecret,
+    access_token_key: creds.oauthKey,
+    access_token_secret: creds.oauthSecret
+  })
+}
+
+const postTweet = (client) => {
+  console.log(client)
+  client
+    .post('statuses/update', {
+      status: 'Please Please Please £1 fish £1 fish'
+    }).catch(console.log)
+}
 
 router.get('/login', async (req, res) => {
   try {
-    const result = await request.post(loginConfig)
-    // options has to come from the config options
-    tmpOauthToken = result.split('&')[0].split('=')[1]
-    tmpOauthTokenSecret = result.split('&')[1].split('=')[1]
-    // Put this all into keys
-    res.json(
-      `https://api.twitter.com/oauth/authorize?oauth_token=${tmpOauthToken}`
-    )
-    // Call this the callback url
+    buildAuthURL().then(URL => res.redirect(URL))
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
@@ -31,79 +84,15 @@ router.get('/success', async (req, res) => {
   try {
     const tmpOauthToken = req.query.oauth_token
     const oauthVerifier = req.query.oauth_verifier
-    // Get these varuables from another function
-    // Store relevant information inside the keys variable
-    sendrequestToTwitter(tmpOauthToken, oauthVerifier, tmpOauthTokenSecret)
-    res.redirect('http://localhost:3001')
-    // Redirect back to where the user wants to be after logging in
+
+    requestTwitterCreds(tmpOauthToken, oauthVerifier, tmpOauthTokenSecret)
+      .then(buildTwitterClient)
+      .then(postTweet)
+
+    res.redirect('/')
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
 })
 
-const sendrequestToTwitter = async (
-  tmpOauthToken,
-  oauthVerifier,
-  tmpOauthTokenSecret
-) => {
-  const requestConfig = {
-    headers: {
-      Accept: '*/*',
-      Connection: 'close',
-      'User-Agent': 'node-twitter/1'
-    },
-    oauth: {
-      consumer_key: consumerKey,
-      consumer_secret: consumerSecret,
-      token: tmpOauthToken,
-      token_secret: tmpOauthTokenSecret,
-      verifier: oauthVerifier
-    },
-    url: 'https://api.twitter.com/oauth/access_token'
-  }
-  const result = await request
-    .post(requestConfig)
-    .then(result => createTwitterSesssion(result))
-    .then(client => postTweet(client))
-    .catch(console.log)
-  return result
-}
-
-const createTwitterSesssion = async result => {
-  oauthToken = result.split('&')[0].split('=')[1]
-  oauthTokenSecret = result.split('&')[1].split('=')[1]
-  // send all of this into the keys
-  client = new Twitter({
-    consumer_key: consumerKey,
-    consumer_secret: consumerSecret,
-    access_token_key: oauthToken,
-    access_token_secret: oauthTokenSecret
-  })
-}
-
-const postTweet = async () => {
-  const post = await client
-    .post('statuses/update', {
-      status: 'Please Please Please £1 fish £1 fish'
-    })
-    .catch(console.log)
-}
-
-const loginConfig = {
-  headers: {
-    Accept: '*/*',
-    Connection: 'close',
-    'User-Agent': 'node-twitter/1'
-  },
-  oauth: {
-    consumer_key: consumerKey,
-    consumer_secret: consumerSecret,
-    // Comes from the keys folder
-    callback: encodeUrl('http://localhost:3001/twitter/success')
-    // Put in the callback url
-  },
-  url: 'https://api.twitter.com/oauth/request_token'
-}
-
 module.exports = router
-// Store this whole thing as a config object inside the api folder
