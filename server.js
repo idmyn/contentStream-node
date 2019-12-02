d8717dc81c243467903c7fedbbeeb2b97fb659db
idmyn <hello@davidmyno.rs>
2019-12-02 15:06:49 +0000

    allow cors

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
-
 const express = require('express')
 const app = express()
-const PORT = 3001
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 
-mongoose.connect("mongodb://localhost/contentStream", {
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/contentStream', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
-
-app.use(express.json())
 
 const authRouter = require('./routes/auth')
 app.use('/auth', authRouter)
@@ -29,4 +29,5 @@ app.use('/buckets', bucketsRouter)
 const twitterRouter = require('./routes/twitter')
 app.use('/twitter', twitterRouter)
 
+const PORT = 3001
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
