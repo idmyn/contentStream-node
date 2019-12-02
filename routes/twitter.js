a8d838b32afbb3a26d0688a76fc7a39e88280ee6
idmyn <hello@davidmyno.rs>
2019-12-02 15:19:56 +0000

    clear existing twitter routes preparing for refactor

--- routes/twitter.js
+++ routes/twitter.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const API = require('../api/twitter')
 
 router.get('/login', async (req, res) => {
+  // throw error if JWT header missing
   try {
     API.buildAuthURL().then(URL => res.redirect(URL))
   } catch (err) {
@@ -14,32 +15,40 @@ router.get('/login', async (req, res) => {
 router.get('/success', async (req, res) => {
   try {
     API.requestTwitterCreds({ ...req.query })
-      .then(API.buildTwitterClient)
-      .then(API.postTweet)
-    res.redirect('/')
+      .then(console.log)
+    // API.requestTwitterCreds({ ...req.query })
+    //   .then(API.buildTwitterClient)
+    //   .then(client => API.postTweet(client, 'Please Please Please £1 fish £1 fish'))
+    // res.redirect('/twitter/timeline')
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
 })
 
-router.get('/create_tweet', async(req, res) => {
-    try {
-      const post = await client.post('statuses/update', {
-        status: "Status"
-      })
-      .catch(console.log)
-      res.redirect('http://localhost:3001')
-      //Redirect this back to the main page
-    } catch (err) {
-      res.status(500).json({ message: err.message })
-    }
+router.post('/tweet', (req, res) => {
+  try {
+    res.json(req)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+  // try {
+  //   await client.post('statuses/update', {
+  //     status: 'Status'
+  //   }).catch(console.log)
+  //   res.redirect('http://localhost:3001')
+  // } catch (err) {
+  //   res.status(500).json({ message: err.message })
+  // }
 })
 
-router.get('/hometimeline', async(req, res) => {
+router.get('/timeline', async (req, res) => {
+  // try {
+  //   const timeline = await client
+  //     .get('statuses/home_timeline', {})
+  //     .then(timeline => timeline.map(post => post.id_str))
+  //     .then(postIds => res.json(postIds))
   try {
-    const timeline = await client.get('statuses/home_timeline', {})
-    .then(timeline => timeline.map(post => post.id_str))
-    .then(postIds => res.json(postIds))
+    res.json(req)
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
