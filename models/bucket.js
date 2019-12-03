99b0e8841b563e875ecfe8a554866e214d500b0f
idmyn <hello@davidmyno.rs>
2019-12-03 14:57:47 +0000

    add user ref to bucket model

--- models/bucket.js
+++ models/bucket.js
@@ -5,6 +5,11 @@ const bucketSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
 })
 
