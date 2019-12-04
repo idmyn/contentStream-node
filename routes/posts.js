4a06ce30e622fce572902eaefaac0f0c083ce6bd
idmyn <hello@davidmyno.rs>
2019-12-04 14:06:08 +0000

    store bucket id on new posts for now

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const Post = require('../models/post')
+const Bucket = require('../models/bucket')
+
+const serialize = (post) => (
+  {
+    id: post._id,
+    sourceId: post.sourceId,
+    domain: post.domain,
+    buckets: post.buckets
+  }
+)
 
 // index
 router.get('/', async (req, res) => {
@@ -15,23 +25,23 @@ router.get('/', async (req, res) => {
 // create
 router.post('/', async (req, res) => {
   try {
-    const foundPost = Post.findOne({ sourceId: req.body.id })
+    const bucket = await Bucket.findOne({ _id: req.body.bucketId })
+    console.log('bucket:', bucket)
+    const post = new Post({
+      sourceId: req.body.sourceId,
+      domain: req.body.domain,
+      bucket: bucket._id
+    })
+    const newPost = await post.save()
+    console.log('newPost', newPost)
+
+    bucket.posts.push(newPost._id)
+    const savedBucket = await bucket.save()
+    console.log(savedBucket)
+
+    res.status(201).json(newPost)
   } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-
-  // const post = new Post({
-  //   sourceId: req.body.id,
-  //   domain: req.body.domain
-  // })
-
-  // post.buckets.push(req.bucketId)
-
-  try {
-    // const newPost = await post.save()
-    // res.status(201).json(newPost)
-    res.json("post recieved", foundPost)
-  } catch (err) {
+    console.log(err)
     res.status(400).json({ message: err.message })
   }
 })
